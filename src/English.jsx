ed6d9726655cc021596d1823d7f9000bf37731de
Ryudark <steinu037@gmail.com>
2022-09-26 15:30:28 -0500

    more change

--- src/English.jsx
+++ src/English.jsx
@@ -2,7 +2,9 @@ export default function English(){
     return (
     <div>
         <h1>Hi! I'M STEVEN</h1>
-        <div></div>
+        <div>
+            <iframe src="https://www.youtube.com/embed/WVXgthJPaV0" frameborder="0"></iframe>
+        </div>
         <div></div>
     </div>
     )
--- src/Spanish.jsx
+++ src/Spanish.jsx
@@ -1,8 +1,11 @@
 export default function Spanish(){
     return (
       <div>
-        <h1>Hola soy STEVEN</h1>
-        <div></div>
+        <h1>¡Hola! soy STEVEN</h1>
+        <div>
+
+          <iframe src="https://www.youtube.com/embed/WVXgthJPaV0" frameborder="0"></iframe>
+        </div>
         <div></div>
       </div>
     )
