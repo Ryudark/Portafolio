6ae034de9a9b22a9c91b3fcf63e872e47a244c48
Ryudark <steinu037@gmail.com>
2022-09-28 17:31:49 -0500

    cuadro de imagenes

--- src/Spanish.jsx
+++ src/Spanish.jsx
@@ -1,13 +1,24 @@
+import portada from "./images/Portada1.JPG"
+import detail from "./images/gameDetail.JPG"
+import create from "./images/create.JPG"
+import page from "./images/Page1.JPG"
+
+import "./English.css"
+
 export default function Spanish(){
     return (
       <div>
         <h1>¡Hola! soy STEVEN</h1>
         <div>
-          <label>Presentación Clickcare:</label>
+          <label>Clickcare:</label>
           <iframe src="https://www.youtube.com/embed/WVXgthJPaV0" frameborder="0" title="Presentacion Clickcare" />
         </div>
         <div>
-          
+          <label>VideoGames</label>
+          <img src={portada} alt="portada" className="image"/>
+          <img src={detail} alt="detail" className="image"/>
+          <img src={create} alt="create" className="image"/>
+          <img src={page} alt="page" className="image"/>
         </div>
       </div>
     )
